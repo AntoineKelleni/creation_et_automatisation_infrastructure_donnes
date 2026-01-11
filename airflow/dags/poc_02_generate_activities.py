1c2e78782141bca0a1c32a25643aae5b2a645952
Antoine <antoinekelleni@gmail.com>
2026-01-11 20:10:16 +0100

    CRON dayly, TriggerDagRunOperator Quality test

--- airflow/dags/poc_02_generate_activities.py
+++ airflow/dags/poc_02_generate_activities.py
@@ -1,11 +1,12 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime
 
 with DAG(
     dag_id="poc_02_generate_activities",
     start_date=datetime(2025, 1, 1),
-    schedule=None,
+    schedule="0 7 * * *",   # <-- CRON : tous les jours à 07:00
     catchup=False,
 ) as dag:
 
@@ -14,4 +15,12 @@ with DAG(
         bash_command="python /opt/airflow/project/src/transformations/generate_clean_activities.py",
     )
 
-    generate
+    trigger_poc_05 = TriggerDagRunOperator(
+        task_id="trigger_poc_05_quality_checks",
+        trigger_dag_id="poc_05_quality_checks",
+        # optionnel : pour éviter de relancer si déjà déclenché
+        reset_dag_run=True,
+        wait_for_completion=False,
+    )
+
+    generate >> trigger_poc_05
